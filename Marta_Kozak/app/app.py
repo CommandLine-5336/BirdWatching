31a0173f5926428ad676eb497a0d3fd98352d506
Marta Kozak <martakozak94666@gmail.com>
2026-06-20 21:09:56 +0300

    chore(vm):CL-32 write a web page

---
+++ Marta_Kozak/app/app.py
@@ -0,0 +1,33 @@
+import os
+import socket
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(BASE_DIR, 'birds.json')
+
+with open(JSON_PATH, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    BIRDS = data['birds']
+
+bird_index = 0
+
+@app.route('/')
+def index():
+    global bird_index
+    hostname = socket.gethostname()
+
+    selected_bird = BIRDS[bird_index]
+
+    bird_index = (bird_index + 1) % len(BIRDS)
+    
+    return render_template(
+        'index.html', 
+        hostname=hostname, 
+        bird=selected_bird
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

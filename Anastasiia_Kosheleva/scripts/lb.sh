76fc98d75a53d92e4f977f592247e2f8f4e5c70d
anastasiakosh <anastasiakoseleva31@hg47.org>
2026-06-20 15:54:28 +0300

    chore(lb): add load balancer script, root gitignore, and fix history

---
+++ Anastasiia_Kosheleva/scripts/lb.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y && apt-get install nginx -y
+
+cat << 'EOF' > /etc/nginx/sites-available/default
+upstream web_servers {
+    server 192.168.56.11:80;
+    server 192.168.56.12:80;
+}
+server {
+    listen 80;
+    location / {
+        proxy_pass http://web_servers;
+    }
+}
+EOF
+systemctl restart nginx
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# BirdWatching
-Testing ReadME
-# BirdWatching
+﻿# Load Balancer Configuration
+
+* File: lb.sh
+* Role: Nginx Load Balancer (Reverse Proxy).
+* Upstream: web_servers (192.168.56.11, 192.168.56.12).
+* Execution: sudo bash lb.sh

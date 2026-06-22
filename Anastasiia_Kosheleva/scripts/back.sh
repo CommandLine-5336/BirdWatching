d89966584850791e98efc96d7511426587abda83
anastasiakosh <anastasiakoseleva31@hg47.org>
2026-06-22 16:13:40 +0300

    chore: update web page UI, setup scripts and trivy config

--- Anastasiia_Kosheleva/scripts/back.sh
+++ Anastasiia_Kosheleva/scripts/back.sh
@@ -1,29 +1,15 @@
 ﻿#!/bin/bash
-echo ">>> Backend API starting..."
 export DEBIAN_FRONTEND=noninteractive
+
 apt-get update -y
 apt-get install python3 python3-pip python3-venv psycopg2-binary -y
-
 APP_DIR="/opt/birdwatcher"
+mkdir -p $APP_DIR
 cd $APP_DIR
+
 python3 -m venv venv
 ./venv/bin/pip install Flask gunicorn boto3 psycopg2-binary
 
-cat << 'EOF' > /etc/systemd/system/birdwatcher.service
-[Unit]
-Description=Birdwatcher Backend API Service
-After=network.target
+cp /vagrant/configs/birdwatcher.service /etc/systemd/system/birdwatcher.service
 
-[Service]
-User=root
-WorkingDirectory=/opt/birdwatcher
-Environment="PATH=/opt/birdwatcher/venv/bin"
-ExecStart=/opt/birdwatcher/venv/bin/gunicorn --workers 2 --bind 0.0.0.0:5000 app:app
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reload
-systemctl start birdwatcher
-systemctl enable birdwatcher
+systemctl daemon-reload && systemctl restart birdwatcher && systemctl enable birdwatcher

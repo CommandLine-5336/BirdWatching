f764306c65b43d54ea7c2d2efdcf3d7d31cdde19
anastasiakosh <anastasiakoseleva31@hg47.org>
2026-06-22 15:35:39 +0300

    chore: update lb.sh configuration

--- Anastasiia_Kosheleva/scripts/lb.sh
+++ Anastasiia_Kosheleva/scripts/lb.sh
@@ -1,17 +1,8 @@
 ﻿#!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+
 apt-get update -y && apt-get install nginx -y
 
-cat << 'EOF' > /etc/nginx/sites-available/default
-upstream web_servers {
-    server 192.168.56.11:80;
-    server 192.168.56.12:80;
-}
-server {
-    listen 80;
-    location / {
-        proxy_pass http://web_servers;
-    }
-}
-EOF
+cp /vagrant/configs/nginx.conf /etc/nginx/sites-available/default
+
 systemctl restart nginx

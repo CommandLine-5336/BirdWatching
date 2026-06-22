b9e647d39e58853b77e2dba3815c0048f1e0e341
anastasiakosh <anastasiakoseleva31@hg47.org>
2026-06-22 22:13:48 +0300

    fix: correct ports && copying service and installing python

---
+++ Anastasiia_Kosheleva/scripts/web.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update -y && apt-get install python3 python3-venv -y
+
+python3 -m venv /opt/venv
+/opt/venv/bin/pip install Flask gunicorn pymysql
+
+cp /vagrant/configs/birdwatcher.service /etc/systemd/system/birdwatcher.service
+
+systemctl daemon-reload && systemctl enable --now birdwatcher

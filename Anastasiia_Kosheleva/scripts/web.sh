fae6da3d04118e5c4c4838a81ad88f909d7f9882
anastasiakosh <anastasiakoseleva31@hg47.org>
2026-06-26 15:22:51 +0300

    feat(CL-54): add all new features and configure .gitignore

--- Anastasiia_Kosheleva/scripts/web.sh
+++ Anastasiia_Kosheleva/scripts/web.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-
-apt-get update -y && apt-get install python3 python3-venv -y
+apt-get update -y && apt-get install nginx python3 python3-venv -y
 
 python3 -m venv /opt/venv
 /opt/venv/bin/pip install Flask gunicorn pymysql
 
-cp /vagrant/configs/birdwatcher.service /etc/systemd/system/birdwatcher.service
+cat << 'EOF' > /etc/nginx/sites-available/default
+server {
+    listen 80;
+    location / {
+        proxy_pass http://127.0.0.1:5000;
+        proxy_set_header Host $host;
+    }
+}
+EOF
+systemctl restart nginx
 
-systemctl daemon-reload && systemctl enable --now birdwatcher
+cat << 'EOF' > /etc/systemd/system/birdwatcher.service
+[Unit]
+Description=Birdwatcher App
+[Service]
+User=root
+WorkingDirectory=/opt/birdwatcher
+Environment="PATH=/opt/venv/bin"
+ExecStart=/opt/venv/bin/gunicorn --workers 2 --bind 127.0.0.1:5000 app:app
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload && systemctl restart birdwatcher && systemctl enable birdwatcher

268d84d7c244eb9d9fd08089f9547147d230e971
anasia <103381193+anastasiakosh@users.noreply.github.com>
2026-06-28 16:15:12 +0300

    Delete Anastasiia_Kosheleva/scripts/lb.sh

--- Anastasiia_Kosheleva/scripts/lb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update -y && apt-get install nginx -y
-
-cp /vagrant/configs/nginx.conf /etc/nginx/sites-available/default
-
-systemctl restart nginx

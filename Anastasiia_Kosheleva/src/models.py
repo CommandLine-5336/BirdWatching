e99c1546989e0360458bf16cb99e235a9dd5118c
anasia <103381193+anastasiakosh@users.noreply.github.com>
2026-07-01 20:11:07 +0300

    Update models.py

--- Anastasiia_Kosheleva/src/models.py
+++ Anastasiia_Kosheleva/src/models.py
@@ -7,20 +7,16 @@ db = SQLAlchemy()
 
 class User(db.Model):
     """User account model."""
-
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     login = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
-
     posts = db.relationship("Post", backref="author", lazy=True)
 
 
 class Post(db.Model):
     """Post submission model."""
-
     __tablename__ = "post"
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     photo = db.Column(db.String(100))
     description = db.Column(db.String(100))
@@ -29,11 +25,6 @@ class Post(db.Model):
 
 class Like(db.Model):
     """Like interaction model."""
-
     __tablename__ = "like"
-    user_id = db.Column(
-        db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), primary_key=True
-    )
-    post_id = db.Column(
-        db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), primary_key=True
-    )
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), primary_key=True)
